9c506fb665a2795b69a6c31904b3bd95b88bb468
Saheel1410 <80039754+Saheel1410@users.noreply.github.com>
2021-03-11 02:47:26 -0800

    Update Palindrome1.java

--- Palindrome1.java
+++ Palindrome1.java
@@ -1,19 +1,36 @@
 package assesment_02;
 
+import java.util.Scanner;
+
 public class Palindrome1 {
 
 	public static void main(String[] args) {
+		
+		
 		String str = "hannah";
+		String rev = "";
 		char[] palArray = str.toCharArray();
-		for (int i = 0; i < palArray.length; i++) {
-			for (int j = palArray.length-1; j >=0; j--) {
-				if(palArray[i]==palArray[j]) {
-					System.out.println("it is palindrome");
-				}
-				
-			}
-			
+		
+		for (int i = palArray.length-1; i >=0; i--) {
+			rev = rev+palArray[i]+"";
+			//System.out.println(rev);
 		}
+		if(rev.equals(str)) {
+			System.out.println("given string is a palindrome");
+		}
+		else {
+			System.out.println("given string is not a palindrome");
+		}
+		
+//		for (int i = 0; i < palArray.length; i++) {
+//			for (int j = palArray.length-1; j >=0; j--) {
+//				if(palArray[i]==palArray[j]) {
+//					System.out.println("it is palindrome");
+//				}
+//				
+//			}
+//			
+//		}
 
 	}
 
